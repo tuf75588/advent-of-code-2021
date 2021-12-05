75b9c91631b0821abb1f3961ea4608bc3aa00eb5
Andrew Davis <atd285@gmail.com>
2021-12-05 15:45:31 -0500

    refactor day 1 solution

--- solutions/01/index.js
+++ solutions/01/index.js
@@ -1,52 +1,45 @@
 const fs = require('fs');
 const path = require('path');
-const pathname = path.join(__dirname, 'input.txt');
 
-const data = fs.readFileSync(pathname, { encoding: 'utf-8' });
+const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
+  encoding: 'utf-8',
+});
 
-const numList = data.split('\n').map((num) => num * 1);
+const numbers = data.trim().split('\n').map(Number);
 
-// // find how many times we are strictly increasing depth
-function strictlyIncreasing(arr) {
+/**
+ * Named after the purpose in the puzzle, this function takes an array of
+ * numbers and sums 3 numbers at a time in a sliding window. So the first
+ * sum is of indexes 0, 1, and 2, the next sum is indexes 1, 2, 3, the next
+ * sum is indexes 2, 3, 4 and so on.
+ */
+function convertSlidingWindow(numbersArray) {
+  let [a, b, ...otherNumbers] = numbersArray;
+  let c;
+  const sums = [];
+
+  for (const number of otherNumbers) {
+    c = number;
+    const sum = a + b + c;
+    sums.push(sum);
+    a = b;
+    b = c;
+  }
+  return sums;
+}
+
+function increasingCountValues(numbersArray) {
+  const [firstValue, ...otherValues] = numbersArray;
   let count = 0;
-  const isIncreasing = arr.reduce((count, _, index, arr) => {
-    const nextValue = arr[index + 1];
-    if (nextValue > arr[index]) count++;
-    return count;
-  }, count);
-
-  return isIncreasing;
-}
-start at index 1, index 0 has no previous measurement
-
-console.log(strictlyIncreasing(numList));
-
-/* part two */
-
-function addSums(array) {
-  return array
-    .filter((x) => x.length === 3)
-    .map((element, index, arr) => {
-      return element.reduce((accu, curr) => {
-        return accu + curr;
-      }, 0);
-    });
+  let prevValue = firstValue;
+  for (const value of otherValues) {
+    if (value > prevValue) count++;
+    prevValue = value;
+  }
+  return count;
 }
 
-function sliceArray(arr) {
-  // break our array into three windows
-  let data = [];
-  arr.forEach((_, index, array) => {
-    data.push(arr.slice(index, index + 3));
-  });
-  return addSums(data);
-}
+const a = increasingCountValues(convertSlidingWindow(numbers));
+const b = increasingCountValues(numbers);
 
-function compareNextAndPrevious(array) {
-  return array.reduce((accumulator, currentValue, index, arr) => {
-    let count = 0;
-    if (array[index + 1] > array[index]) accumulator++;
-    return accumulator;
-  }, 0);
-}
-console.log(compareNextAndPrevious(sliceArray(numList)));
+console.log({ a, b });
