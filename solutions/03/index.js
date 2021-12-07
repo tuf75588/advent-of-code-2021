112db8451d54ba72b7ee7ac98d0b34ab9ac9c796
Andrew Davis <atd285@gmail.com>
2021-12-07 10:33:10 -0500

    ???

--- solutions/03/index.js
+++ solutions/03/index.js
@@ -5,8 +5,16 @@ const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
   encoding: 'utf-8',
 });
 
-const binList = data.trim().split('\n').map(a => a.split(''));
+const binList = data
+  .trim()
+  .split('\n')
+  .map((a) => a);
 
-console.log(binList);
-
-
+// console.log(binList[0][0]);
+// console.log(binList[1][0]);
+// console.log(binList[2][0]);
+// console.log(binList[3][0]);
+// console.log(binList[4][0]);
+// console.log(binList[5][0]);
+// console.log(binList[6][0]);
+const r = binList.reduce((acc, curr, indx, arr) => {}, {});
