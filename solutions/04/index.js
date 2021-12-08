8ba4f78042ce98e7ed2ed59581c5273192f36d89
Andrew Davis <atd285@gmail.com>
2021-12-08 16:52:33 -0500

    more work on day four problem

--- solutions/04/index.js
+++ solutions/04/index.js
@@ -5,8 +5,25 @@ const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
   encoding: 'utf-8',
 });
 
-const [numStr, ...board] = data.split('\n\n');
+const [numStr, ...boards] = data.split('\n\n');
 const numbers = numStr.split(',').map(Number);
-console.log(numbers);
 
-function parseBoard(board) {}
+function parseBoard(board) {
+  const parsedBoard = board
+    .split('\n')
+    .filter(Boolean)
+    .map((row) => {
+      row
+        .split(/\s/)
+        .filter(Boolean)
+        .map(Number)
+        .map((num) => ({ value: num, isMarked: false }));
+    });
+  const index = {};
+  for (const item of parsedBoard.flat()) {
+    index[item.value] = item;
+  }
+  return { board: parsedBoard, index };
+}
+
+parseBoard(numbers);
