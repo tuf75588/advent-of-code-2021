1c77195d1f91322749c63501035eb4d560f030ac
Andrew Davis <atd285@gmail.com>
2021-12-11 23:05:24 -0500

    answer for part 1 day 5

--- solutions/05/index.js
+++ solutions/05/index.js
@@ -24,15 +24,24 @@ function applyLinesToGrid(lines) {
   const maxY = Math.max(...lines.flatMap(({ y1, y2 }) => [y1, y2]));
 
   const grid = generateEmptyGrid(maxX + 1, maxY + 1);
+
+  for (const line of lines) {
+    const points = generatePointsForLine(line);
+
+    for (const [col, row] of points) {
+      grid[row][col]++;
+    }
+  }
+  return grid;
 }
 
-function generatePointsForLines({ x1, y1, x2, y2 }) {
+function generatePointsForLine({ x1, y1, x2, y2 }) {
   const result = [];
   const xDiff = x1 - x2;
   const yDiff = y1 - y2;
 
   if (!xDiff) {
-    for (let i = 0; i < Math.abs(yDiff); i++) {
+    for (let i = 0; i <= Math.abs(yDiff); i++) {
       const nextY = yDiff < 0 ? y1 + i : y1 - i;
       result.push([x1, nextY]);
     }
@@ -40,8 +49,16 @@ function generatePointsForLines({ x1, y1, x2, y2 }) {
   }
   if (!yDiff) {
     for (let i = 0; i <= Math.abs(xDiff); i++) {
-      const nextX = xDiff > 0 ? x1 + i : x1 - i;
+      const nextX = xDiff < 0 ? x1 + i : x1 - i;
       result.push([nextX, y1]);
+    }
+    return result;
+  }
+  if (Math.abs(xDiff / yDiff)) {
+    for (let i = 0; i <= Math.abs(xDiff); i++) {
+      const nextX = xDiff < 0 ? x1 + i : x1 - i;
+      const nextY = yDiff < 0 ? y1 + i : y1 - i;
+      result.push([nextX, nextY]);
     }
     return result;
   }
@@ -55,3 +72,11 @@ function generateEmptyGrid(cols, rows) {
 function getLineIntersections(grid) {
   return grid.flat().filter((item) => item > 1);
 }
+
+const horizontalLines = lines.filter(({ x1, x2 }) => x1 === x2);
+const verticalLines = lines.filter(({ y1, y2 }) => y1 === y2);
+const nonDiagonalLines = [...horizontalLines, ...verticalLines];
+const linedGrid = applyLinesToGrid(nonDiagonalLines);
+const intersections = getLineIntersections(linedGrid);
+const firstAnswer = intersections.length;
+console.log(firstAnswer);
