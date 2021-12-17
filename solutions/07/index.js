6b66788df2e2173c42af401606ed47912743a8ba
Andrew Davis <atd285@gmail.com>
2021-12-17 16:08:10 -0500

    answer part 1 day 7

--- solutions/07/index.js
+++ solutions/07/index.js
@@ -5,13 +5,25 @@ const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
   encoding: 'utf-8',
 });
 
-// const positions = data.trim().split('')
-// console.log(positions)
+const positions = data.split(',').map((a) => parseInt(a.trim()));
 
-let n = 0;
+/* find the most re-occuring integer in this array */
 
-while (n < 3) {
-  n++;
+const largestPosition = Math.max(...positions);
+function getMax(object) {
+  return Object.keys(object).filter((x) => {
+    return object[x] == Math.max.apply(null, Object.values(object));
+  });
 }
 
-console.log(n);
+let n = 0;
+let sums = [];
+
+
+let getFuelCost = (x) => positions.map((element) => (element > x ? element - x : x - element)).reduce((a,b) => a + b);
+
+while (n < largestPosition) {
+    sums.push(getFuelCost(n));
+    n++
+}
+console.log(Math.min(...sums));
