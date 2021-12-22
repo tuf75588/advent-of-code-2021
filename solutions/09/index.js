fa4492b89ff68c3b75f734afc69491eaf892bf97
Andrew Davis <atd285@gmail.com>
2021-12-22 15:36:26 -0500

    day 9 part two solution

--- solutions/09/index.js
+++ solutions/09/index.js
@@ -36,4 +36,68 @@ function partOne(map) {
 }
 
 const firstAnswer = partOne(data); // 228
-console.log(firstAnswer);
+
+/* part two */
+const makePoint = (rowIdx, colIdx) => [rowIdx, colIdx];
+const p = makePoint;
+
+function partTwo(map) {
+  const matrix = createMatrix(map);
+
+  function getCell(rowIdx, colIdx, guard) {
+    return guard ? matrix[rowIdx][colIdx] : Infinity;
+  }
+
+  // we are finding low coords for the second part of this problem
+  const lowPoints = [];
+  for (const [rowIndex, row] of matrix.entries()) {
+    for (const [colIdx, col] of row.entries()) {
+      const above = getCell(rowIndex - 1, colIdx, rowIndex > 0);
+      const right = getCell(rowIndex, colIdx + 1, colIdx < row.length - 1);
+      const below = getCell(rowIndex + 1, colIdx, rowIndex < matrix.length - 1);
+      const left = getCell(rowIndex, colIdx - 1, colIdx > 0);
+
+      if (col < above && col < right && col < below && col < left) {
+        lowPoints.push(p(rowIndex, colIdx));
+      }
+    }
+  }
+
+  const visited = {};
+
+  function walkBasin(point) {
+    const [rowIdx, colIdx] = point;
+    const key = `${rowIdx}-${colIdx}`;
+
+    if (visited[key]) return [];
+    visited[key] = true;
+
+    const row = matrix[rowIdx];
+
+    if (!row) return [];
+
+    const col = row[colIdx];
+
+    if (col === undefined || col === 9) return [];
+
+    return [
+      col,
+      ...walkBasin(p(rowIdx + 1, colIdx)),
+      ...walkBasin(p(rowIdx, colIdx + 1)),
+      ...walkBasin(p(rowIdx - 1, colIdx)),
+      ...walkBasin(p(rowIdx, colIdx - 1)),
+    ];
+  }
+  const basins = lowPoints.map((lowPoint) => walkBasin(lowPoint));
+  const sizes = basins.map((basin) => basin.length);
+  const sortedSizes = [...sizes].sort((a, b) => {
+    if (a < b) return 1;
+    if (a > b) return -1;
+    return 0;
+  });
+  const threeLargest = sortedSizes.slice(0, 3);
+  const product = threeLargest.reduce((acc, curr) => acc * curr, 1);
+  return product;
+}
+
+const secondAnswer = partTwo(data); // 1113424
